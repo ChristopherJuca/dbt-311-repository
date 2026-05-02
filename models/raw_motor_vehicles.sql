dd09cd0dac8f01540bb37c1104b3fd7e03416ca8
Christopher Juca <christopherjuca@gmail.com>
2026-05-02 02:01:31 +0000

    new

--- models/raw_motor_vehicles.sql
+++ models/raw_motor_vehicles.sql
@@ -1,3 +1,3 @@
 {{config (materialized='table')}}
 
-select * from `cis-4400-311-project.nyc_311_raw.motor_vehicles_raw_data`
+select * from `cis-4400-311-project.nyc_311_raw.motor_vehicle_raw_data`
