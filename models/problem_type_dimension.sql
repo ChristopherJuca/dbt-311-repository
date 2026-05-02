f74672357c6718fd6cdcbe4d8c543b4fbf29b3f8
Christopher Juca <christopherjuca@gmail.com>
2026-05-02 00:31:07 +0000

    new

---
+++ models/problem_type_dimension.sql
@@ -0,0 +1,17 @@
+{{ config(materialized='table') }}
+
+WITH problem_type_columns AS (
+  SELECT DISTINCT
+    problem_type,
+    problem_detail
+  FROM {{ ref('raw_311_complaints') }}
+  WHERE problem_type IS NOT NULL
+)
+
+SELECT
+  ROW_NUMBER() OVER () AS problem_type_dimension_id,
+  problem_type,
+  problem_detail,
+  CURRENT_TIMESTAMP() AS loaded_at
+FROM problem_type_columns
+ORDER BY problem_type, problem_detail
