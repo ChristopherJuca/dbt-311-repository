006304d37c5a3560050cfb5317cbb60d726f6381
Christopher Juca <christopherjuca@gmail.com>
2026-05-02 00:21:21 +0000

    new

--- models/agency_dimension.sql
+++ models/agency_dimension.sql
@@ -1,16 +1,15 @@
-WITH agency_columns AS
-(
+{{ config(materialized='table') }}
+
+WITH agency_columns AS (
   SELECT DISTINCT
-    agency,
     agency_name
   FROM {{ ref('raw_311_complaints') }}
-  WHERE agency IS NOT NULL
+  WHERE agency_name IS NOT NULL
 )
 
 SELECT
   ROW_NUMBER() OVER () AS agency_name_dimension_id,
-  agency,
   agency_name,
   CURRENT_TIMESTAMP() AS loaded_at
 FROM agency_columns
-ORDER BY agency, agency_name
+ORDER BY agency_name
