05f5480f58aede3d04d8e5b992c685e92ccb9093
christopher juca <christopherjuca@gmail.com>
2026-05-16 01:35:27 +0000

    new vehicle sql

--- models/vehicle_type_dimension.sql
+++ models/vehicle_type_dimension.sql
@@ -1,23 +1,22 @@
 {{config(materialized = 'table')}}
 
 WITH vehicle_type_columns AS (
-    select distinct 
-        vehicle_type_code_1,
-        vehicle_type_code_2
-        
-    from {{ref('cleaned_motor_vehicles')}}
 
-    where vehicle_type_code_1 IS NOT NULL
+    SELECT DISTINCT
+        vehicle_type_code_1
+
+    FROM {{ref('cleaned_motor_vehicles')}}
+
+    WHERE vehicle_type_code_1 IS NOT NULL
 
 )
 
-select 
-    row_number() over() AS vehicle_type_dimension_id,
-    vehicle_type_code_1, vehicle_type_code_2,
+SELECT
+    ROW_NUMBER() OVER() AS vehicle_type_dimension_id,
+    vehicle_type_code_1,
 
     CURRENT_TIMESTAMP() AS loaded_at
 
-    from vehicle_type_columns
-    ORDER BY vehicle_type_code_1,vehicle_type_code_2
+FROM vehicle_type_columns
 
-
+ORDER BY vehicle_type_code_1
